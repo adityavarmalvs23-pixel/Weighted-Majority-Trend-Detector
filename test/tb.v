c4c7d8521a5c39eade8ed3ecc669853e2c49de1d
adityavarmalvs23-pixel <adityavarma.lvs23@rvce.edu.in>
2025-07-30 21:53:49 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,38 +1,71 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+    reg clk, rst_n, ena;
+    reg [7:0] ui_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_in, uio_out, uio_oe;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+`ifdef GL_TEST
+    supply1 VPWR;
+    supply0 VGND;
+`endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    tt_um_weighted_majority dut (
+`ifdef GL_TEST
+        .VPWR(VPWR),
+        .VGND(VGND),
+`endif
+        .clk(clk),
+        .rst_n(rst_n),
+        .ena(ena),
+        .ui_in(ui_in),
+        .uo_out(uo_out),
+        .uio_in(uio_in),
+        .uio_out(uio_out),
+        .uio_oe(uio_oe)
+    );
+
+    assign uio_in = 8'd0;
+
+    always #5 clk = ~clk;
+
+    initial begin
+        $dumpfile("tb.vcd");
+        $dumpvars(0, tb);
+        clk = 0;
+        rst_n = 1;
+        ena = 1;
+        ui_in = 8'b00000000;
+
+        #20 rst_n = 0;
+        #20 rst_n = 1;
+
+        // Send '0' bits
+        repeat (4) begin
+            ui_in[0] = 0;
+            #10;
+        end
+
+        // Send '1' bits
+        repeat (5) begin
+            ui_in[0] = 1;
+            #10;
+        end
+
+        // Send '0' bits again
+        repeat (6) begin
+            ui_in[0] = 0;
+            #10;
+        end
+
+        #100 $finish;
+    end
+
+    //initial begin
+    //    $monitor("T=%0t | ui_in[0]=%b | uo_out[0]=%b", $time, ui_in[0], uo_out[0]);
+    //end
 
 endmodule
